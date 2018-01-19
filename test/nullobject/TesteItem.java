7802d6f2011be9ec10b0ba9d0de3920f0805e3f1
allan-mlpe <allan_mlpe@hotmail.com>
2018-01-19 20:50:11 -0300

    Refactor nos testes do padrão Null Object

--- test/nullobject/TesteItem.java
+++ test/nullobject/TesteItem.java
@@ -4,24 +4,24 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestItem {
+public class TesteItem {
 
     @Test
-    public void testSemDesconto() {
+    public void testeSemDesconto() {
         Item i = new Item("tenis", 150.00);
         assertEquals(300.00, i.precoQuantidade(2), 0.001);
         assertEquals("tenis R$150,00", i.toString());
     }
 
     @Test
-    public void testDescontoPercentual() {
+    public void testeDescontoPercentual() {
         Item i = new Item("tenis", 150.00, new DescontoPercentual(10));
         assertEquals(270.0, i.precoQuantidade(2), 0.001);
         assertEquals("tenis R$135,00", i.toString());
     }
 
     @Test
-    public void testDescontoAbsoluto() {
+    public void testeDescontoAbsoluto() {
         Item i = new Item("tenis", 150.00, new DescontoAbsoluto(10));
         assertEquals(280.0, i.precoQuantidade(2), 0.001);
         assertEquals("tenis R$140,00", i.toString());
