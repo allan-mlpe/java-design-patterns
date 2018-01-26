14e42e19da5befa155ac54ad084eb0a320670947
allan-mlpe <allan_mlpe@hotmail.com>
2018-01-25 23:21:42 -0300

    Inclusão de teste de cenário com voos compostos por conexões e escalas

--- test/composite/TesteTrechosCompostos.java
+++ test/composite/TesteTrechosCompostos.java
@@ -36,4 +36,19 @@ public class TesteTrechosCompostos {
         assertEquals(430, t3.getCusto());
         assertEquals(600, t3.getDistancia());
     }
+
+    @Test
+    public void trechoCompostoEscalaConexao() {
+        TrechoAereo t1 = new TrechoAereoSimples("REC", "BSB", 200, 200);
+        TrechoAereo t2 = new TrechoAereoSimples("BSB", "GRU", 350, 200);
+        TrechoAereo t3 = new TrechoAereoComEscala(t1, t2);
+        TrechoAereo t4 = new TrechoAereoSimples("GRU", "NAT", 400, 400);
+        TrechoAereo conexao = new TrechoAereoConexao(t3, t4, 100);
+
+        assertEquals("REC", conexao.getOrigem());
+        assertEquals("NAT", conexao.getDestino());
+        assertEquals(1050, conexao.getCusto());
+        assertEquals(800, conexao.getDistancia());
+
+    }
 }
